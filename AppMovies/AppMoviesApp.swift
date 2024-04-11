58f707f0f2e739b1333e1267e51b7a8680f13b96
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-04-11 09:13:15 -0500

    init

---
+++ AppMovies/AppMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMoviesApp.swift
+//  AppMovies
+//
+//  Created by Christopher Peralta on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AppMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
